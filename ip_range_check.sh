e0c19a381477f261d00eaf627dee17cac9b990b9
Ricardo MONLA <rmonla@gmail.com>
2023-06-13 15:21:48 -0300

    Compatibility with shell + Detection of IPs with multiple MAC addresses
    The suggested improvement to the script aims to make it work with shell (sh) and includes the ability to detect if an IP has multiple MAC addresses for security alerts.
    
    Compatibility with shell (sh): The original script was modified to ensure that it works correctly in a more basic shell environment. This involved avoiding bash-specific features and using syntax that is compatible with sh.
    
    Detection of IPs with multiple MAC addresses: The script intends to use the arping command to retrieve the MAC address associated with a specific IP and then print the IP:MAC combination. By repeating this process for each MAC address found, we can identify if an IP has multiple associated MAC addresses.
    
    With this improvement, the script becomes more portable and enables the detection of potential security issues related to IP and MAC addresses.

--- ip_range_check.sh
+++ ip_range_check.sh
@@ -1,38 +1,33 @@
-#!/bin/bash
+#!/bin/sh
 
-set -x #debug mode
-log_file=~/arping-test-result_"$(date +%X)".log
+# set -x # Modo de depuración
+log_file=~/arping-test-result_$(date +%X).log
 
-function usage() {
-   echo -e "Usage: $(basename ${0}) --start IPADDR --end IPADDR"
-   echo -e "  OR   $(basename ${0}) -s IPADDR -e IPADDR"
-   1>&2; exit 1;
+usage() {
+   echo "Usage: $(basename "$0") --start IPADDR --end IPADDR"
+   echo "  OR   $(basename "$0") -s IPADDR -e IPADDR"
+   exit 1
 }
 
-
-function ip_to_int() {
-  local IP="${1}"
-  local A=$(echo ${IP} | cut -d. -f1)
-  local B=$(echo ${IP} | cut -d. -f2)
-  local C=$(echo ${IP} | cut -d. -f3)
-  local D=$(echo ${IP} | cut -d. -f4)
-  local INT
-
-  INT=$(expr 256 "*" 256 "*" 256 "*" ${A})
-  INT=$(expr 256 "*" 256 "*" ${B} + ${INT})
-  INT=$(expr 256 "*" ${C} + ${INT})
-  INT=$(expr ${D} + ${INT})
-
-  echo ${INT}
+ip_to_int() {
+  IP="$1"
+  A=$(echo "$IP" | cut -d. -f1)
+  B=$(echo "$IP" | cut -d. -f2)
+  C=$(echo "$IP" | cut -d. -f3)
+  D=$(echo "$IP" | cut -d. -f4)
+  INT=$((256 * 256 * 256 * A))
+  INT=$((256 * 256 * B + INT))
+  INT=$((256 * C + INT))
+  INT=$((D + INT))
+  echo "$INT"
 }
 
-function int_to_ip() {
-  local INT="${1}"
-  local D=$(expr ${INT} % 256)
-  local C=$(expr '(' ${INT} - ${D} ')' / 256 % 256)
-  local B=$(expr '(' ${INT} - ${C} - ${D} ')' / 65536 % 256)
-  local A=$(expr '(' ${INT} - ${B} - ${C} - ${D} ')' / 16777216 % 256)
-
+int_to_ip() {
+  INT="$1"
+  D=$((INT % 256))
+  C=$(((INT - D) / 256 % 256))
+  B=$(((INT - C - D) / 65536 % 256))
+  A=$(((INT - B - C - D) / 16777216 % 256))
   echo "${A}.${B}.${C}.${D}"
 }
 
@@ -42,14 +37,14 @@ fi
 
 while true;
 do
-    case "${1}" in
+    case "$1" in
       -s | --start)
           shift
-          ip_start=${1}
+          ip_start="$1"
       ;;
       -e | --end)
           shift
-          ip_end=${1}
+          ip_end="$1"
       ;;
       -h | ? | --help)
           usage
@@ -61,27 +56,30 @@ do
     shift
 done
 
-# IP Addr Number Representation
-ip_start_num=$(ip_to_int ${ip_start})
-ip_end_num=$(ip_to_int ${ip_end})
+# Representación numérica de la dirección IP
+ip_start_num=$(ip_to_int "$ip_start")
+ip_end_num=$(ip_to_int "$ip_end")
 
-# clean log file.
-if [ -e ${log_file} ];then
-   rm -f ${log_file}
+# Limpiar el archivo de registro.
+if [ -e "$log_file" ]; then
+   rm -f "$log_file"
 fi
 
-# arping it.
+# Realizar arping.
 mac_regex='([0-9A-F]{2}[:-]){5}([0-9A-F]{2})'
-offset=$(expr ${ip_end_num} - ${ip_start_num})
-for i in $(seq 0 ${offset});
+offset=$((ip_end_num - ip_start_num))
+for i in $(seq 0 "$offset");
 do
-    ip_num=$(expr ${ip_start_num} + ${i})
-    ip=$(int_to_ip ${ip_num})
-    arping -c 1 ${ip} | tee -a ${log_file}
+    ip_num=$((ip_start_num + i))
+    ip=$(int_to_ip "$ip_num")
+
+    for mac in $(arping -c 1 "$ip" | grep -Eo "$mac_regex"); do
+      echo "$ip<=>$mac" | tee -a "$log_file"
+    done
     sleep 1
 done
 
-registered_ips_num=$(cat ${log_file} | egrep ${mac_regex} | wc -l)
-echo -e "\n************************************************" | tee -a ${log_file}
-echo -e "Scanning is finished." |  tee -a ${log_file}
-echo -e "Total Scanned IPs=$(expr ${offset} + 1), where Registered IPs=${registered_ips_num}" | tee -a ${log_file}
+registered_ips_num=$(grep -E "$mac_regex" "$log_file" | wc -l)
+echo "\n************************************************" | tee -a "$log_file"
+echo "Scanning is finished." |  tee -a "$log_file"
+echo "Total Scanned IPs=$((offset + 1)), where Registered IPs=$registered_ips_num" | tee -a "$log_file"
